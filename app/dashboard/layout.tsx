18de86a429170a2479b0f5948828638c32fb9d3b
brian-ambani <brainambani1@gmail.com>
2024-02-03 03:28:54 +0300

    updated the layout file this the children

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col space-y-6 mt-10">
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <h1>Hello</h1>
+        </aside>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
+}
