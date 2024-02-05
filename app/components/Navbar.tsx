d10534196f2d1247e73aa6d3f4e7c3216088d7aa
brian-ambani <brainambani1@gmail.com>
2024-02-05 12:50:11 +0300

    updated the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,7 +14,9 @@ export const Navbar = async () => {
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <h1 className="font-bold text-3xl">BrainSaas</h1>
+          <h1 className="font-bold text-3xl">
+            Brain<span className="text-primary">Saas</span>
+          </h1>
         </Link>
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
